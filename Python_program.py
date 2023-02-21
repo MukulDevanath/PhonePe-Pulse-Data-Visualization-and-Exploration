4573afd982b48acc21452d15f2dc17a26225d8ca
MukulDevanath <117461612+MukulDevanath@users.noreply.github.com>
2023-02-21 12:06:59 +0530

    Update Python_program.py

--- Python_program.py
+++ Python_program.py
@@ -3,7 +3,6 @@ import mysql.connector as connection
 import pandas as pd
 import plotly.express as px
 
-
 col1, col2, col3 = st.columns(3)
 
 with col1:
@@ -15,28 +14,46 @@ with col3:
 
 col4, col5 = st.columns(2)
 
+option1 = 'Total'
+option2 = 'Users'
+
 with col4:
-    option1 = st.selectbox("Select Year", ("2018", "2019", "2020", "2021", "2022"))
+    option1 = st.selectbox("Select Year", ("Total", "2018", "2019", "2020", "2021", "2022"))
 with col5:
     option2 = st.selectbox("Users or Transactions", ("Users", "Transactions"))
 
+option1 = option1.lower()
+option2 = option2.lower()
 
-db_connection = connection.connect(host="localhost", database='PhonePe', user="root", password="Ferrari488Pi!")
+db_connection = connection.connect(host="localhost", database='PhonePe', user="root", password="**********")
 db_cursor = db_connection.cursor()
-db_cursor.execute('SELECT * FROM test')
+db_cursor.execute('SELECT * FROM users')
 table_rows = db_cursor.fetchall()
 df = pd.DataFrame(table_rows)
 
-df.to_csv("test.csv", header=['state', 'users'])
+df.to_csv("users.csv", header=['state', '2018', '2019', '2020', '2021', '2022', 'total'])
 
-df = pd.read_csv("test.csv")
+db_cursor2 = db_connection.cursor()
+db_cursor2.execute('SELECT * FROM transactions')
+table_rows = db_cursor2.fetchall()
+df2 = pd.DataFrame(table_rows)
+
+df2.to_csv("transactions.csv", header=['state', '2018', '2019', '2020', '2021', '2022', 'total'])
+
+display_dataframe = pd.DataFrame()
+if option2 == 'users':
+    display_dataframe = pd.read_csv("users.csv")
+else:
+    display_dataframe = pd.read_csv("transactions.csv")
+
+display_dataframe = display_dataframe[['state', option1]]
 
 fig = px.choropleth(
-    df,
+    display_dataframe,
     geojson="https://gist.githubusercontent.com/jbrobst/56c13bbbf9d97d187fea01ca62ea5112/raw/e388c4cae20aa53cb5090210a42ebb9b765c0a36/india_states.geojson",
     featureidkey='properties.ST_NM',
     locations='state',
-    color='users',
+    color=option1,
     color_continuous_scale='Blues'
 )
 
